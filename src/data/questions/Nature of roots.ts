ba32a73ec86a58ccaf3c645778b3a945ee065b4d
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-04-15 14:32:55 -0500

    Add nature of roots questions data

--- src/data/questions/Nature of roots.ts
+++ src/data/questions/Nature of roots.ts
@@ -1,1 +1,8 @@
 
+import type { RawQuestionRow } from "./types";
+
+export const natureOfRootsQuestions: RawQuestionRow[] = [
+  [52, 52, "Nature Of Roots", "The roots of the equation 2𝑥² − 𝑥 + 1 = 0 are", "real and equal", "real and distinct", "not real and equal", "not real and distinct"],
+  [53, 53, "Nature Of Roots", "The roots of the equation 5𝑥² + 6𝑥 − 2 = 0 are", "not real and not distinct", "not real and not equal", "real and distinct", "real and equal"],
+  [54, 54, "Nature Of Roots", "The roots of the equation 3𝑥² − 6𝑥 − 5 = 0 are", "equal", "real and distinct", "distinct and not real", "real and not distinct"],
+];
