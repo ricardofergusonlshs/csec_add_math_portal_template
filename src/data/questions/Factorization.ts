cab87e192050aecc6e0269ecb979377cd05ff9f8
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-04-15 14:21:50 -0500

    Update Factorization.ts

--- src/data/questions/Factorization.ts
+++ src/data/questions/Factorization.ts
@@ -1,1 +1,10 @@
 
+import type { RawQuestionRow } from "./types";
+
+export const FactorizationQuestions: RawQuestionRow[] = [
+  [1, 1, "Factorization", "𝑎(𝑏 + 𝑐) − 𝑏(𝑎 + 𝑐) is equal to", "𝑎(𝑐 − 𝑏)", "𝑎(𝑏 − 𝑐)", "𝑐(𝑎 − 𝑏)", "𝑐(𝑏 − 𝑎)"],
+  [2, 2, "Factorization", "The expression ab + 3c − 3b − ac is equal to", "(𝑎 + 3)(𝑐 − 𝑏)", "(𝑎 + 3)(𝑏 − 𝑐)", "(𝑎 − 3)(𝑏 + 𝑐)", "(𝑎 − 3)(𝑏 − 𝑐)"],
+  [3, 3, "Factorization", "The expression ab + 3c − 3b − ac is equal to", "(𝑎 + 3)(𝑐 − 𝑏)", "(𝑎 + 3)(𝑏 − 𝑐)", "(𝑎 − 3)(𝑏 − 𝑐)", "(𝑎 − 3)(𝑏 + 𝑐)"],
+  [4, 4, "Factorization", "1/(𝑥 + 3) + 3/(𝑥² − 9) expressed as a single fraction is:", "𝑥/(𝑥² − 9)", "(𝑥² − 6)/(𝑥² − 9)(𝑥 + 3)", "(𝑥 + 6)/(𝑥² − 9)", "4/(𝑥 + 3)"],
+  [5, 5, "Factorization", "The expression 𝑝𝑞 + 5𝑟 − 5𝑞 − 𝑝𝑟 is equal to", "(𝑝 − 5)(𝑞 − 𝑟)", "(𝑝 + 5)(𝑟 − 𝑞)", "(𝑝 + 5)(𝑞 − 𝑟)", "(𝑝 − 5)(𝑞 + 𝑟)"],
+];
