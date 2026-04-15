9d2f3939299f2ef42e7b984614e2aad9ebb4327b
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-04-15 14:36:11 -0500

    Add linear and quadratic inequality questions

--- src/data/questions/linear & Quadratic Inequality.ts
+++ src/data/questions/linear & Quadratic Inequality.ts
@@ -1,1 +1,15 @@
 
+import type { RawQuestionRow } from "./types";
+
+export const linearQuadraticInequalityQuestions: RawQuestionRow[] = [
+  [74, 74, "Linear & Quadratic Inequality", "The range of values for which 2𝑥² < 5𝑥 + 3 𝑖𝑠", "−1/2 < 𝑥 < 3", "1/2 < 𝑥 < 3", "𝑥 < −1/2 𝑎𝑛𝑑 𝑥 < 3", "𝑥 > −1/2 𝑎𝑛𝑑 𝑥 > 3"],
+  [75, 75, "Linear & Quadratic Inequality", "The values of x which satisfy the inequality (2𝑥 – 3)/(𝑥 + 1)> 0 are", "𝑥 > −1 𝑎𝑛𝑑 𝑥 > 3/2", "𝑥 > 3/2", "𝑥 < −1 𝑜𝑟 𝑥 > 3/2", "𝑥 > −1"],
+  [76, 76, "Linear & Quadratic Inequality", "The set of values of x for which 5𝑥 + 7 > 10𝑥 − 13 is", "𝑥 < −4", "𝑥 ≥ −4", "𝑥 < 4", "𝑥 > 4"],
+  [77, 77, "Linear & Quadratic Inequality", "The range of values of x for which 5𝑥 + 6 ≤ 𝑥² is", "{𝑥∶ −3 ≤ 𝑥 ≤ −2}", "{𝑥∶ 𝑥 ≤ −1} ∪ {𝑥∶ 𝑥 ≥ 6}", "{𝑥∶ 𝑥 ≥ −1} ∪ {𝑥∶ 𝑥 ≥ 6}", "{𝑥∶ −1 ≤ 𝑥 ≤ 6}"],
+  [78, 78, "Linear & Quadratic Inequality", "The range of values for which 𝑥² − 7𝑥 + 10 < 0 is", "2 > 𝑥 > 5", "2 < 𝑥 < 5", "𝑥 < 2 𝑎𝑛𝑑 𝑥 > 5", "𝑥 < −5 𝑎𝑛𝑑 𝑥 > −5"],
+  [79, 79, "Linear & Quadratic Inequality", "The set of values of x for which (5x − 2)/(2 − 3x) ≥ 0 is given by:", "{x : x ≥ 2/5 ∪ x > 2/3}", "{x : x ≤ 2/5 ∪ x > 2/3}", "{x : 2/3 < x ≤ 2/5}", "{x : 2/5 ≤ x < 2/3}"],
+  [80, 80, "Linear & Quadratic Inequality", "Given that x > 0, the set of values of x for which 𝑥 – 2 < 15/𝑥 is:", "(A){𝑥∶ 𝑥 > 0 ∪ 𝑥 > 5}", "(B){𝑥∶ 0 < 𝑥 < 5}", "(C){𝑥∶ 𝑥 > 5}", "(D){𝑥∶ 𝑥 < 5}"],
+  [81, 81, "Linear & Quadratic Inequality", "The values of x for which (𝑥 + 15)² = 64𝑥 are", "3 𝑎𝑛𝑑 5", "9 𝑎𝑛𝑑 5", "3 𝑎𝑛𝑑 25", "9 𝑎𝑛𝑑 25"],
+  [82, 82, "Linear & Quadratic Inequality", "The values of x for which 3𝑥 – 2/2𝑥 + 1 ≤ 0 are", "𝑥 ≤ 2/3", "𝑥 > − 1/2", "− 1/2 < 𝑥 ≤ 2/3", "𝑥 ≤ − 1/2 𝑜𝑟 𝑥 ≥ 2/3"],
+  [83, 83, "Linear & Quadratic Inequality", "The range of values of x for which 4𝑥 − 3𝑥² > 0 is", "− 4/3 < 𝑥 < 0", "𝑥 < 0, 𝑥 > 4/3", "0 < 𝑥 < 4/3", "𝑥 > 0, 𝑥 > 4/3"],
+];
