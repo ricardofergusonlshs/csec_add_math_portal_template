bb27ea6ff20d15381efac968fa7762c8489ea8e9
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-04-15 14:37:29 -0500

    Add trigonometry questions data

--- src/data/questions/Trigonometry.ts
+++ src/data/questions/Trigonometry.ts
@@ -1,1 +1,27 @@
 
+import type { RawQuestionRow } from "./types";
+
+export const trigonometryQuestions: RawQuestionRow[] = [
+  [97, 97, "Trigonometry", "cos(A − B) − cos(A + B) =", "2 sin A sin B", "−2 sin A cos B", "2 cos A sin B", "2 cos A cos B"],
+  [98, 98, "Trigonometry", "If sin 𝜃= 15/17 and θ is obtuse, then cos θ is equal to", "−8/15", "−8/17", "8/15", "8/17"],
+  [99, 99, "Trigonometry", "The smallest positive angle for which the equation 𝑠𝑖𝑛 𝜃 + 𝑐𝑜𝑠 𝜃 = 0 is", "𝜋/4", "3𝜋/4", "5𝜋/4", "7𝜋/4"],
+  [100, 100, "Trigonometry", "For 0 ≤ θ ≤ 2π, solutions for the equation 4 sin² θ − 1 = 0 exist in", "1, 2 and 3", "1, 3 and 4", "2, 3 and 4", "1, 2, 3 and 4"],
+  [101, 101, "Trigonometry", "2 sin (𝑥 − 𝜋/2)is equal to", "2 sin 𝑥− 2", "−2 cos 𝑥", "2 cos (𝑥 + 𝜋/2)", "2 sin 𝑥− 𝜋"],
+  [102, 102, "Trigonometry", "For which of the following ranges of values is 𝑓(𝑥) = 2 + 𝑐𝑜𝑠 3𝑥 valid?", "−1 ≤ 𝑓(𝑥) ≤ 3", "1 ≤ 𝑓(𝑥) ≤ 1", "−2 ≤ 𝑓(𝑥) ≤ 2", "0 ≤ 𝑓(𝑥) ≤ 2"],
+  [103, 103, "Trigonometry", "For 0 ≤ x ≤ 2π, the values of x which satisfy the equation 2 𝑐𝑜𝑠² 𝑥 + 3 𝑠𝑖𝑛 𝑥 = 0 are", "𝑥 = 𝜋/6 , 𝑥 = 5𝜋/6", "𝑥 = 𝜋/6 , 𝑥 = − 5𝜋/6", "𝑥 = 7𝜋/6 , 𝑥 = 11𝜋/6", "𝑥 = 5𝜋/6 , 𝑥 = 7𝜋/6"],
+  [104, 104, "Trigonometry", "If sin 𝜃= 5/13 and θ is obtuse, then tan θ =", "−12/13", "−5/12", "5/12", "12/13"],
+  [105, 105, "Trigonometry", "𝑜𝑠(A + B) + 𝑐𝑜𝑠(A − B) =", "2 cos A", "2 cos A + 2 cos B", "cos² A cos² B", "2 cos A cos B"],
+  [106, 106, "Trigonometry", "If 𝑐𝑜𝑠 2𝑥 = 1 − 2𝑠², then sin x =", "s", "s²", "2s", "1 − s²"],
+  [107, 107, "Trigonometry", "The exact value of tan 150° is given by", "−1/√3", "1/√3", "−√3", "√3"],
+  [108, 108, "Trigonometry", "The graph of y = sin 2x is", "Graph A", "Graph B", "Graph C", "Graph D"],
+  [109, 109, "Trigonometry", "The SMALLEST positive angle for which the equation 𝑠𝑖𝑛 𝜃 − 𝑐𝑜𝑠 𝜃 = 0 𝑓𝑜𝑟 0 ≤ 𝜃 ≤ 2𝜋 is", "𝜋/6", "𝜋/4", "5𝜋/6", "2𝜋/3"],
+  [110, 110, "Trigonometry", "𝑠𝑖𝑛 (𝛼 + 45°) is equal to", "1/√2 (sin 𝛼+ cos 𝛼)", "1/√2 (cos 𝛼−sin 𝛼)", "1/2 (sin 𝛼−cos 𝛼)", "1/2 (cos 𝛼−sin 𝛼)"],
+  [111, 111, "Trigonometry", "Convert 4𝜋/5 radians into degrees.", "72", "144", "180", "288"],
+  [112, 112, "Trigonometry", "The trigonometrical expression 𝑆𝑖𝑛𝑥/(1 − 𝑐𝑜𝑠𝑥)− 𝑠𝑖𝑛𝑥/(1 + 𝑐𝑜𝑠𝑥) is identical to", "2sin𝑥", "2tan𝑥", "2/sin 𝑥", "2/𝑡𝑎𝑛²𝑥"],
+  [113, 113, "Trigonometry", "The EXACT value of cos ( 5𝜋/12) is:", "1/4 (√6 − √2)", "1/4 (√6 + √2)", "1/2 (√6 + √2)", "1/2 (√6 − √2)"],
+  [114, 114, "Trigonometry", "The graph shown represents the function:", "𝑐𝑜𝑠 𝑥", "𝑐𝑜𝑠 2𝑥", "1/2 cos 𝑥", "𝑐𝑜𝑠(1/2 𝑥)"],
+  [115, 115, "Trigonometry", "𝑠𝑖𝑛 50° 𝑐𝑜𝑠 40° − 𝑐𝑜𝑠 50° 𝑠𝑖𝑛 40° =", "sin 10°", "cos 10°", "sin 90°", "cos 90°"],
+  [116, 116, "Trigonometry", "The size of angle x = 20 degrees, measured in radians, is:", "𝜋/12", "𝜋/9", "𝜋/6", "𝜋/3"],
+  [117, 117, "Trigonometry", "sin(𝜋/2 − 𝑥) + cos(𝑥 + 𝜋/2) =", "tan𝜋", "sin𝑥 − cos𝑥", "cos𝑥 − sin𝑥", "1 − sin𝑥 − cos𝑥"],
+  [118, 118, "Trigonometry", "If 𝑠𝑖𝑛(90° − 𝑥) = 𝑐𝑜𝑠 𝑥, then the value of 𝑥 is:", "35°", "45°", "60°", "70°"],
+];
