c6eb5d455d72c43823bcb67af16545db5b56e502
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-04-15 14:32:14 -0500

    Rename file to 'Sum and Product of Roots.ts'

---
+++ src/data/questions/Sum and Product of Roots.ts
@@ -0,0 +1,8 @@
+
+import type { RawQuestionRow } from "./types";
+
+export const sumProductOfRootsQuestions: RawQuestionRow[] = [
+  [49, 49, "Sum/Product Of Roots", "Given that a and b are the roots of the equation 𝑥² + 3𝑥 + 4 = 0, what is the value of (𝑎 + 𝑏)²?", "9/16", "1", "9", "16"],
+  [50, 50, "Sum/Product Of Roots", "A quadratic equation is such that the sum of its roots is −2/3 and the product of its roots is 3/4. The quadratic equation is:", "12x² + 8x + 9 = 0", "12x² − 8x − 9 = 0", "12x² − 8x + 9 = 0", "12x² + 8x − 9 = 0"],
+  [51, 51, "Sum/Product Of Roots", "The quadratic equation is:", "12𝑥² + 8𝑥 + 9 = 0", "12𝑥² − 8𝑥 − 9 = 0", "12𝑥² − 8𝑥 + 9 = 0", "12𝑥² + 8𝑥 − 9 = 0"],
+];
