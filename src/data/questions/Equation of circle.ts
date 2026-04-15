3272ce44eebb82a81e556fea6a39e78720929b9b
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-04-15 14:36:36 -0500

    Add equation of circle questions to dataset

--- src/data/questions/Equation of circle.ts
+++ src/data/questions/Equation of circle.ts
@@ -1,1 +1,10 @@
 
+import type { RawQuestionRow } from "./types";
+
+export const equationOfACircleQuestions: RawQuestionRow[] = [
+  [84, 84, "Equation Of A Circle", "The point (2, 3) is at one end of a diameter of the circle whose equation is 𝑥² + 𝑦² − 10𝑥 + 2𝑦 + 1 = 0. The coordinates of the other end of the diameter are", "(−12, −5)", "(−12, −1)", "(8, −1)", "(8, −5)"],
+  [85, 85, "Equation Of A Circle", "The radius, r, and the coordinates of the centre, C, of the circle with equation 𝑥² + 𝑦² − 6𝑥 + 4𝑦 − 12 = 0 are", "𝑟 = 5, 𝐶(−2, 3)", "𝑟 = 25, 𝐶(2, −3)", "𝑟 = 12, 𝐶(−3, 2)", "𝑟 = 5, 𝐶(3, −2)"],
+  [86, 86, "Equation Of A Circle", "The coordinates of the points A and B are (2, -3) and (-10, -5) respectively. The perpendicular bisector to the line AB is given by the equation:", "𝑥 − 6𝑦 + 20 = 0", "6𝑥 + 𝑦 + 28 = 0", "𝑥 + 6𝑦 − 20 = 0", "6𝑥 + 𝑦 − 28 = 0"],
+  [87, 87, "Equation Of A Circle", "The coordinates of the centre of a circle with equation (𝑥 − 1)² + (𝑦 + 3)² = 36 is", "(1, −3)", "(−1, 3)", "(3, −1)", "(−3, 1)"],
+  [88, 88, "Equation Of A Circle", "A circle C has centre (3, -2) and radius 4. The equation of C is", "x^2 + y^2 + 6x - 4y + 3 = 0", "x^2 + y^2 - 3 = 0", "x^2 + y^2 - 6x + 4y - 3 = 0", "x^2 + y^2 + 3x - 2y - 3 = 0"],
+];
