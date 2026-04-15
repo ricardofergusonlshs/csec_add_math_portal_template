63e7b9efe1a2c4000dcb0006f7c8728564a0c7ac
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-04-15 14:41:46 -0500

    Add statistics questions to the dataset

--- src/data/questions/statistics.ts
+++ src/data/questions/statistics.ts
@@ -1,1 +1,9 @@
+import type { RawQuestionRow } from "./types";
 
+export const statisticsQuestions: RawQuestionRow[] = [
+  [150, 150, "Statistics", "There are 108 cards in a deck of UNO playing cards. There are four “Wild”, four “Wild Draw Four” and 25 each of four colours (red, yellow, green, blue). If a player needs to select 1 card, what is the probability of NOT drawing a “Wild Draw Four”?", "26/27", "25/108", "2/27", "1/108"],
+  [151, 151, "Statistics", "One advantage of using a box and whisker plot is that:", "the variance can be identified", "all data values can be identified", "the mean value can be identified", "the spread of the distribution can be identified."],
+  [152, 152, "Statistics", "What is the value of p(N or G)' given that p(N) = 0.40, p(G) = 0.15 and the p(N and G) = 0.35?", "0.10", "0.35", "0.90", "1.00"],
+  [153, 153, "Statistics", "What is P(N | G)?", "0.10", "0.40", "0.47", "0.70"],
+  [154, 154, "Statistics", "The tree diagram above shows the probability of Events A and B occurring. Based on the diagram, the value of P(A | B) =", "1/5", "8/15", "3/5", "4/5"],
+];
