ee9ee5506ca3323b0127b914aa57928d4b73cb90
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-04-15 14:35:38 -0500

    Add surd questions to the question set

--- src/data/questions/surd.ts
+++ src/data/questions/surd.ts
@@ -1,1 +1,11 @@
 
+import type { RawQuestionRow } from "./types";
+
+export const surdQuestions: RawQuestionRow[] = [
+  [68, 68, "Surd", "The expression (1 + √3)/(√3 − 1) when simplified is equal to", "-1", "1", "(√3+ 2)/2", "√3 + 2"],
+  [69, 69, "Surd", "(8 + √5)(2 − √5) can be expressed as", "11 − 6√5", "21 − 6√5", "11 + 6√5", "11 + 10√5"],
+  [70, 70, "Surd", "The expression (√5− 1)/(1 + √5) when simplified is equal to", "1/3(3 − √5)", "1/2(√5 − 3)", "1/3 (√5 − 3)", "1/2 (3 − √5)"],
+  [71, 71, "Surd", "The expression (1 + √3)/(√3− 1) when simplified is equal to:", "-1", "1", "√3+ 2/2", "√3 + 2"],
+  [72, 72, "Surd", "The value of √18 + √50 is", "34√2", "6√15", "8√2", "√68"],
+  [73, 73, "Surd", "4/(√6− 2)", "2(√6 − 2)", "2(√6 + 2)", "√6 − 2", "√6 + 2"],
+];
