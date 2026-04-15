2d9e537b5cfa680623e5d3616ea156efeefa8612
ricardofergusonlshs <ricardofergusonlshs@gmail.com>
2026-04-15 14:38:09 -0500

    Add radianQuestions data for semicircle problem

--- src/data/questions/Radian.ts
+++ src/data/questions/Radian.ts
@@ -1,1 +1,6 @@
 
+import type { RawQuestionRow } from "./types";
+
+export const radianQuestions: RawQuestionRow[] = [
+  [119, 119, "Radian", "If the area of the semicircle is 32π, what is the length of the arc connecting points A and B?", "4π", "8π", "16π", "32π"],
+];
